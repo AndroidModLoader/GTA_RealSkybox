c5f50b2e66e7190f99fe677910e36b49d628024d
RusJJ <mctesting1@yandex.ru>
2024-06-05 12:07:34 +0300

    Update Application.mk

--- Application.mk
+++ Application.mk
@@ -1,5 +1,5 @@
 APP_STL := c++_static
-APP_ABI := armeabi-v7a arm64-v8a # 64-bit version is stable but the texture is plain blue.
+APP_ABI := armeabi-v7a arm64-v8a
 APP_OPTIM := release
 APP_PLATFORM := android-17
 NDK_TOOLCHAIN_VERSION := clang
